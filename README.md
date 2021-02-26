2c69cf1c7572fdfad606edbf977a3033937dcd66
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-02-25 21:10:48 -0300

    Creating README file and refactoring files

---
+++ README.md
@@ -0,0 +1,50 @@
+# A Simple To-do List
+
+This app is a simple to-do list made by me with the goal of learning how to manipulate the DOM and test my JavaScript skills.
+
+## :toolbox: Built With
+
+- JavaScript
+- HTML
+- CSS
+
+## :rocket: How To Start
+
+1. Clone the repo
+
+```sh
+  $ git clone https://github.com/thiagocrux/todo-list.git
+```
+
+2. Open the HTML file on you browser
+
+## :camera: Screenshots
+
+![screenshot-1](https://user-images.githubusercontent.com/29850573/109230721-11afae00-77a4-11eb-93c4-3abcc183597c.png)
+![screenshot-2](https://user-images.githubusercontent.com/29850573/109230746-1e340680-77a4-11eb-8d50-2fd1dddaafd2.png)
+
+## :alarm_clock: Status
+
+The app is lacking some features like responsivity and data persistence and this might be added in the future
+
+## :pirate_flag: Licence
+
+Copyright (c) 2021 by Thiago Luiz da Cruz Souza
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- app.js
+++ app.js
@@ -1,199 +1,151 @@
 let todos = [];
 
 let DOM = {
-    list: document.querySelector('.list'),
-    input: document.querySelector('.input'),
-    addButton: document.querySelector('.add-new-task'),
-    deleteButton: document.querySelector('.delete'),
-    checkButton: document.querySelector('.check'),
-    deleteAllButton: document.querySelector('.delete-all'),
+  list: document.querySelector('.list'),
+  input: document.querySelector('.input'),
+  addButton: document.querySelector('.add-new-task'),
+  deleteButton: document.querySelector('.delete'),
+  checkButton: document.querySelector('.check'),
+  deleteAllButton: document.querySelector('.delete-all'),
 };
-
-/***** FUNÇÕES *****/
-
-/* Função que cria o template dos itens da lista de tarefas. */
 
 const createItem = (el) => {
-    const markup = `
-        <li class="list-item">
-            <p class="todo">${el.todo}</p>
-            <div class="buttons-container">
-                <button class="delete button hide">
-                    <i class="large material-icons">clear</i>
-                </button>
-                <button class="checkbox button">
-                    ${el.done === false ? '<i class="large material-icons">check_box_outline_blank</i>' : '<i class="large material-icons">check_box</i>'}
-                </button>
-            </div>
-        </li>
-    `; 
-    
-    DOM.list.insertAdjacentHTML('beforeend', markup);
-}
+  const markup = `
+		<li class="list-item">
+				<p class="todo">${el.todo}</p>
+				<div class="buttons-container">
+						<button class="delete button hide">
+								<i class="large material-icons">clear</i>
+						</button>
+						<button class="checkbox button">
+							${
+                el.done === false
+                  ? '<i class="large material-icons">check_box_outline_blank</i>'
+                  : '<i class="large material-icons">check_box</i>'
+              }
+						</button>
+				</div>
+		</li>
+	`;
 
-/* Função que renderiza os itens na tela. */
-
-const renderItems = () => {
-    todos.forEach((el) => {
-        createItem(el);
-    });
+  DOM.list.insertAdjacentHTML('beforeend', markup);
 };
 
-/* Função que recebe o valor do input. */
+const renderItems = () => todos.forEach((el) => createItem(el));
 
 const getInput = () => DOM.input.value;
 
-/* Função que limpa o valor do input. */
+const clearInput = () => (DOM.input.value = '');
 
-const clearInput = () => DOM.input.value = '';
-
-/* Customiza as linhas, diferenciando-as por cores. */
-
-const customizeLines = () => {
-    document.querySelectorAll('.list-item').forEach((el, i) => {
-        if (i % 2 === 0) {
-            el.classList.add('custom-item-background-1');
-        } else {
-            el.classList.add('custom-item-background-2');
-        }
-    });
-};
-
-/* Função que customiza as tarefas, diferenciando-as por checagem. */
+const customizeLineBackground = () =>
+  document.querySelectorAll('.list-item').forEach((el, i) => {
+    return i % 2 === 0
+      ? el.classList.add('custom-item-background-1')
+      : el.classList.add('custom-item-background-2');
+  });
 
 const customizeTodos = () => {
-    const allTodos = document.querySelectorAll('.todo');
+  const allTodos = document.querySelectorAll('.todo');
 
-    allTodos.forEach((el, i) => {
-        if (todos[i].done) {
-            el.classList.add('checked');
-        }
-        else {
-            el.classList.remove('checked');
-        }
-    });
-}
-
-/* Função que limpa a interface do usuário, evitando itens duplicados. */
-
-const clearUI = () => DOM.list.innerHTML = '';
-
-/* Função que adiciona os itens à interface do usuário. */
-
-const updateUI = () => {
-    // Limpa as entradas anteriores da interface do usuário.
-    clearUI();
-
-    // Limpa o input.
-    clearInput();
-
-    // Cria os itens que serão adicionados à interface de usuário.
-    renderItems();
-    
-    // Personaliza o estilo das tarefas caso elas estejam marcadas ou não.
-    customizeTodos();
-
-    // Personaliza/alterna as cores das linhas da lista de tarefas.
-    customizeLines();
+  allTodos.forEach((el, i) =>
+    todos[i].done ? el.classList.add('checked') : el.classList.remove('checked')
+  );
 };
 
-/***** EVENT LISTENTERS *****/
+const clearUI = () => (DOM.list.innerHTML = '');
 
-/* Adiciona uma nova tarefa ao clicar no botão. */
+const updateUI = () => {
+  clearUI();
+  clearInput();
+  renderItems();
+  customizeTodos();
+  customizeLineBackground();
+};
+
+// TODO: Implement this method
+const toggleTodo = (todo, todoIndex, todoToBeChecked) => {
+  if (todo.isSameNode(todoToBeChecked)) {
+    {
+      todos[todoIndex].done = !todos[todoIndex].done;
+      updateUI();
+    }
+  }
+};
+
+const deleteTodo = (todo, todoIndex, todoToBeDeleted) => {
+  if (todo.isSameNode(todoToBeChecked)) {
+    {
+      todos.splice(todoIndex, 1);
+      updateUI();
+    }
+  }
+};
 
 DOM.addButton.addEventListener('click', () => {
-    if (DOM.input.value !== '') {
-        todos.push({
-            todo: getInput(),
-            done: false,
-        });
-    
-        updateUI();
+  if (DOM.input.value !== '') {
+    todos.push({
+      todo: getInput(),
+      done: false,
+    });
 
-        clearInput();
-
-        DOM.input.focus();
-    } else {
-        console.log('Erro: Não é possível adicionar tarefas em branco.');
-    }
+    updateUI();
+    clearInput();
+    DOM.input.focus();
+  } else {
+    console.log('Erro: Não é possível adicionar tarefas em branco.');
+  }
 });
-
-/* Adiciona uma nova tarefa ao apertar no ENTER. */
 
 DOM.input.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13 && DOM.input.value !== '') {
-        e.preventDefault();
+  if (e.keyCode === 13 && DOM.input.value !== '') {
+    e.preventDefault();
 
-        todos.push({
-            todo: getInput(),
-            done: false,
-        });
-    
-        updateUI();
+    todos.push({
+      todo: getInput(),
+      done: false,
+    });
 
-        DOM.input.focus();
-    } else {
-        console.log('Erro: Não é possível adicionar tarefas em branco.');
-    }
-});
-
-/* Delegação de eventos para o ícone de exclusão e para a caixa de seleção. */
-
-DOM.list.addEventListener('click', (e) => {
-    const allListItems = document.querySelectorAll('.list-item');
-    const checkbox = e.target.closest('.checkbox');
-    const deleteButton = e.target.closest('.delete');
-
-    // Ao clicar na caixa de seleção...
-    if (checkbox) {
-        const listItemToBeChecked = checkbox.parentNode.parentNode;
-        
-        allListItems.forEach((el, i) => {
-            if (el.isSameNode(listItemToBeChecked)) {{
-                todos[i].done = !todos[i].done;
-                updateUI();
-            }}
-        });
-    }
-    // Ao clicar no botão de deletar...
-    else if (deleteButton) {
-        const listItemToBeRemoved = deleteButton.parentNode.parentNode;
-        
-        allListItems.forEach((el, i) => {
-            if (el.isSameNode(listItemToBeRemoved)) {{
-                todos.splice(i, 1);
-                updateUI();
-            }}
-        });
-    }
-});
-
-/* Exclui todas as tarefas ao clicar no botão. */
-
-DOM.deleteAllButton.addEventListener('click', () => {
-    todos = [];
     updateUI();
     DOM.input.focus();
+  } else {
+    console.log('Erro: Não é possível adicionar tarefas em branco.');
+  }
 });
 
-/* Mostra o ícone de exclusão de uma tarefa específica ao colocar o cursor sobre a linha desta tarefa. */
+DOM.list.addEventListener('click', (e) => {
+  const allListItems = document.querySelectorAll('.list-item');
+  const checkbox = e.target.closest('.checkbox');
+  const deleteButton = e.target.closest('.delete');
+
+  if (checkbox) {
+    const listItemToBeChecked = checkbox.parentNode.parentNode;
+    allListItems.forEach((el, i) => toggleTodo(el, i, listItemToBeChecked));
+  } else if (deleteButton) {
+    const listItemToBeRemoved = deleteButton.parentNode.parentNode;
+    allListItems.forEach((el, i) => deleteTodo(el, i, listItemToBeRemoved));
+  }
+});
+
+DOM.deleteAllButton.addEventListener('click', () => {
+  todos = [];
+  updateUI();
+  DOM.input.focus();
+});
 
 DOM.list.addEventListener('mouseover', (e) => {
-    const listItem = e.target.closest('.list-item');
-    
-    if (listItem) {
-        listItem.children[0].classList.toggle('hover');
-        listItem.children[1].children[0].classList.toggle('hide');
-    }
+  const listItem = e.target.closest('.list-item');
+
+  if (listItem) {
+    listItem.children[0].classList.toggle('hover');
+    listItem.children[1].children[0].classList.toggle('hide');
+  }
 });
 
-/* Esconde o ícone de exclusão de uma tarefa específica ao retirar o cursor da linha desta tarefa. */
-
 DOM.list.addEventListener('mouseout', (e) => {
-    const listItem = e.target.closest('.list-item');
-    
-    if (listItem) {
-        listItem.children[0].classList.toggle('hover');
-        listItem.children[1].children[0].classList.toggle('hide');
-    }
+  const listItem = e.target.closest('.list-item');
+
+  if (listItem) {
+    listItem.children[0].classList.toggle('hover');
+    listItem.children[1].children[0].classList.toggle('hide');
+  }
 });
